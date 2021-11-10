e4624f233e6fcc9a179b4cf01dbd24eb70b81f33
Aybor <borisov_aleks@mail.ru>
2021-11-10 22:37:31 +0300

    Упрощена модель и админка, в модель MyUser добавлено поле confirmation_code

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,21 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import (
-    BaseUserManager, AbstractUser
+    AbstractUser, UserManager
 )
 
 
-class MyUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **kwargs):
-        if not email:
-            raise ValueError('У пользователя должен быть email')
-
-        user = self.model(email=email, **kwargs)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password, **kwargs):
-        user = self.create_user(email=email, is_staff=True, is_superuser=True, **kwargs)
+class MyUserManager(UserManager):
+    def create_superuser(self, email, username, password=None, **kwargs):
+        user = self.create_user(
+            email=email,
+            username=username,
+            is_staff=True,
+            is_superuser=True,
+            **kwargs
+        )
         user.is_admin = True
         user.set_password(password)
         user.save(using=self._db)
@@ -23,17 +20,9 @@ class MyUserManager(BaseUserManager):
 
 
 class MyUser(AbstractUser):
-    email = models.EmailField(
-        verbose_name='email адрес',
-        max_length=255,
-        unique=True,
-    )
     bio = models.TextField(max_length=1000, blank=True)
-    is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    confirmation_code = models.CharField(max_length=20, default=None, blank=True, null=True)
 
     ROLE_CHOICE = [
         ('user', 'Пользователь'),
