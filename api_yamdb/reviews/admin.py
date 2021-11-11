20b8f78e401077499c6dbdf2f04a6ffec0dfdcfd
Татьяна Овчинникова <tanja-ovc@yandex.ru>
2021-11-11 19:07:11 +0300

    adding all models including Genre_Title

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import Comments, Review
+from .models import Category, Comment, Genre, Genre_Title, Review, Title
 
+admin.site.register(Category)
+admin.site.register(Genre)
+admin.site.register(Genre_Title)
+admin.site.register(Title)
 admin.site.register(Review)
-admin.site.register(Comments)
+admin.site.register(Comment)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -4,9 +4,49 @@ from django.db.models.constraints import UniqueConstraint
 from users.models import MyUser as User
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.DateField(blank=True, null=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, related_name='titles',
+        blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre_Title(models.Model):
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE, related_name='genres'
+    )
+    genre = models.ForeignKey(
+        Genre, on_delete=models.CASCADE, related_name='titles'
+    )
+
+
 class Review(models.Model):
     title = models.ForeignKey(
-        Titles, on_delete=models.CASCADE, related_name='reviews'
+        Title, on_delete=models.CASCADE, related_name='reviews'
     )
     text = models.TextField()
     author = models.ForeignKey(
@@ -21,14 +61,14 @@ class Review(models.Model):
         constraints = (
             UniqueConstraint(
                 fields=('author', 'title',), name='unique_review'
-            )
+            ),
         )
 
     def __str__(self):
         return self.text
 
 
-class Comments(models.Model):
+class Comment(models.Model):
     review = models.ForeignKey(
         Review, on_delete=models.CASCADE, related_name='comments'
     )
