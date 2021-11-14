e49dee282d7cbc27774c95677ab1c45f4daff9ae
Vasilii <babas.ka@yandex.ru>
2021-11-14 23:58:08 +0300

    add review and comments serializers and views

--- api_yamdb/api/serializers/__init__.py
+++ api_yamdb/api/serializers/__init__.py
@@ -1,8 +1,11 @@
 from .auth_serializers import SendConfirmationCodeSerializer, CompareConfirmationCodesSerializer
 from .user_serializer import MyUserSerializer
+from .review_comment_serializers import ReviewSerializer, CommentSerializer
 
 __all__ = [
     'SendConfirmationCodeSerializer',
     'CompareConfirmationCodesSerializer',
     'MyUserSerializer',
+    'ReviewSerializer',
+    'CommentSerializer',
 ]
--- api_yamdb/api/views/__init__.py
+++ api_yamdb/api/views/__init__.py
@@ -2,10 +2,13 @@ from .auth_user_view import (MyUserViewSet,
                              send_confirmation_code,
                              compare_confirmation_code,
                              UserAPI,)
+from .review_comment_view import ReviewViewSet, CommentViewSet
 
 __all__ = [
     'MyUserViewSet',
     'send_confirmation_code',
     'compare_confirmation_code',
     'UserAPI',
+    'ReviewViewSet',
+    'CommentViewSet',
 ]
