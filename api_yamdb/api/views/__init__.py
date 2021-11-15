552a496749b445d2d78b1a6ace5ac704aef8c1c0
Татьяна Овчинникова <tanja-ovc@yandex.ru>
2021-11-15 18:46:04 +0300

    commit after resolving merging conflicts - 2

--- api_yamdb/api/views/__init__.py
+++ api_yamdb/api/views/__init__.py
@@ -1,26 +1,17 @@
 from .auth_user_view import (MyUserViewSet,
                              send_confirmation_code,
-<<<<<<< HEAD
                              compare_confirmation_code,
                              UserAPI,)
-from .review_comment_view import ReviewViewSet, CommentViewSet
-=======
-                             compare_confirmation_code)
 from .review_comment_view import ReviewViewSet, CommentViewSet
 from .title_view import TitleViewSet
 from .category_view import CategoryList, CategoryDetail
 from .genre_view import GenreList, GenreDetail
->>>>>>> many-to-many/test
 
 __all__ = [
     'MyUserViewSet',
     'send_confirmation_code',
     'compare_confirmation_code',
-<<<<<<< HEAD
     'UserAPI',
-    'ReviewViewSet',
-    'CommentViewSet',
-=======
     'ReviewViewSet',
     'CommentViewSet',
     'TitleViewSet',
@@ -28,5 +19,4 @@ __all__ = [
     'CategoryDetail',
     'GenreList',
     'GenreDetail',
->>>>>>> many-to-many/test
 ]
