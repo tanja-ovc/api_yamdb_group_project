63317ff339c43dc7cf49ba550482c52922a8f40a
Татьяна Овчинникова <tanja-ovc@yandex.ru>
2021-11-15 20:19:57 +0300

    still resolving merging conflicts

--- api_yamdb/api/serializers/__init__.py
+++ api_yamdb/api/serializers/__init__.py
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 from .auth_serializers import SendConfirmationCodeSerializer, CompareConfirmationCodesSerializer
 from .user_serializer import MyUserSerializer
-=======
-from .auth_serializers import (SendConfirmationCodeSerializer,
-                               CompareConfirmationCodesSerializer)
 from .category_serializer import CategorySerializer
 from .genre_serializer import GenreSerializer
->>>>>>> many-to-many/test
 from .review_comment_serializers import ReviewSerializer, CommentSerializer
 from .title_serializer import TitleSerializerRead, TitleSerializerWrite
-from .user_serializer import MyUserSerializer
 
 __all__ = [
     'SendConfirmationCodeSerializer',
@@ -17,12 +11,8 @@ __all__ = [
     'MyUserSerializer',
     'ReviewSerializer',
     'CommentSerializer',
-<<<<<<< HEAD
-]
-=======
     'TitleSerializerRead',
     'TitleSerializerWrite',
     'CategorySerializer',
     'GenreSerializer',
 ]
->>>>>>> many-to-many/test
