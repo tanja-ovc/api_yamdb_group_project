00c134bd7a7da52d411b7d003985d5c69fddae50
Aybor <borisov_aleks@mail.ru>
2021-11-16 00:08:59 +0300

    Исправлен срок жизни токена

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,7 +1,7 @@
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-from datetime import datetime
+import datetime
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
