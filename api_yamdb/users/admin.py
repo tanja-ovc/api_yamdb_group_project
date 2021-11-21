b965cc20cf8dcc7b4292d6a6f3b2f6ac85192f4e
Татьяна Овчинникова <tanja-ovc@yandex.ru>
2021-11-21 21:09:45 +0300

    isorting again

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
-
 from users.models import CustomUser
 
 
