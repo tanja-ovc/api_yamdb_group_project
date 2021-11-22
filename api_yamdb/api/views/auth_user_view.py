4b251a820d5d3afabe6f082a8503a1189dd9b08e
Vasily <babas.ka@yandex.ru>
2021-11-22 16:04:17 +0300

    Update after review

--- api_yamdb/api/views/auth_user_view.py
+++ api_yamdb/api/views/auth_user_view.py
@@ -26,51 +26,51 @@ def generate_confirmation_code():
 @api_view(['POST'])
 def send_confirmation_code(request):
     serializer = SendConfirmationCodeSerializer(data=request.data)
-    if serializer.is_valid(raise_exception=True):
-        email = serializer.validated_data.get('email')
-        username = serializer.validated_data.get('username')
-        if username == 'me':
-            return Response(
-                'Пользователя с таким именем создать нельзя',
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        if (
-                CustomUser.objects.filter(
-                    Q(email=email) | Q(username=username)
-                )
-        ):
-            return Response(
-                'Такой email уже зарегистрирован',
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        confirmation_code = generate_confirmation_code()
-        CustomUser.objects.update_or_create(
-            defaults={
-                'confirmation_code': make_password(
-                    confirmation_code,
-                    salt=None,
-                    hasher='argon2'
-                )
-            },
-            email=email,
-            username=username,
+    serializer.is_valid(raise_exception=True)
+    email = serializer.validated_data.get('email')
+    username = serializer.validated_data.get('username')
+    if username == 'me':
+        return Response(
+            'Пользователя с таким именем создать нельзя',
+            status=status.HTTP_400_BAD_REQUEST
         )
-        subject = 'Your confirmation code for YaMDb'
-        message = f'Код подтверждения {confirmation_code}'
-        send_mail(subject, message, PROJECT_SETTINGS['support_email'], [email])
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    if (
+            CustomUser.objects.filter(
+                Q(email=email) | Q(username=username)
+            )
+    ):
+        return Response(
+            'Такой email уже зарегистрирован',
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    confirmation_code = generate_confirmation_code()
+    CustomUser.objects.update_or_create(
+        defaults={
+            'confirmation_code': make_password(
+                confirmation_code,
+                salt=None,
+                hasher='argon2'
+            )
+        },
+        email=email,
+        username=username,
+    )
+    subject = 'Your confirmation code for YaMDb'
+    message = f'Код подтверждения {confirmation_code}'
+    send_mail(subject, message, PROJECT_SETTINGS['support_email'], [email])
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
 def compare_confirmation_code(request):
     serializer = CompareConfirmationCodesSerializer(data=request.data)
-    if serializer.is_valid(raise_exception=True):
-        username = serializer.validated_data.get('username')
-        confirmation_code = serializer.validated_data.get('confirmation_code')
-        user = get_object_or_404(CustomUser, username=username)
-        if check_password(confirmation_code, user.confirmation_code):
-            return Response({'token': f'{AccessToken.for_user(user)}'})
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+    serializer.is_valid(raise_exception=True)
+    username = serializer.validated_data.get('username')
+    confirmation_code = serializer.validated_data.get('confirmation_code')
+    user = get_object_or_404(CustomUser, username=username)
+    if check_password(confirmation_code, user.confirmation_code):
+        return Response({'token': f'{AccessToken.for_user(user)}'})
+    return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class CustomUserViewSet(viewsets.ModelViewSet):
@@ -102,11 +102,12 @@ class CustomUserViewSet(viewsets.ModelViewSet):
                     data=request.data,
                     partial=True
                 )
-                if serializer.is_valid(raise_exception=True):
-                    serializer.validated_data.pop('role', None)
-                    serializer.save()
-                    return Response(serializer.data, status=status.HTTP_200_OK)
+                serializer.is_valid(raise_exception=True)
+                serializer.validated_data.pop('role', None)
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
             return Response(
                 'Для изменения необходимо авторизоваться',
                 status=status.HTTP_401_UNAUTHORIZED
             )
+
